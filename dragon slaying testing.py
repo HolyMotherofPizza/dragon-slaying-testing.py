6aac4781d5be069a1e2dbb7f5664efec762c2779
HolyMotherofPizza <q8mind@hotmail.com>
2014-10-28 22:39:28 +0300

    updated

--- dragon slaying testing.py
+++ dragon slaying testing.py
@@ -8,23 +8,20 @@ class Dragon:
             if self.health <= 0 :
                 print '%s is dead' %self.name
                 return False
+                break
             elif C[target.name].health <=0:
                 return False
+                break
             else:
-                if self.health > 0 or C[target.name].health > 0:
-                    if C[target.name].health <= 50:
+                if self.health > 0:
                         hit = randint(1,20)
                         target.health -= hit
                         print '%s hits %s for %s' %(self.name,target.name,hit)
-                    elif C[target.name].health > 50:
-                        hit = randint(1,50)
-                        if hit >= 40:
-                            C[target.name].health -= hit
-                            print "%s critically hits %s for %s" %(self.name,target.name,hit)
-                        else:
-                            C[target.name].health -= hit
-                            print "%s hits %s for %s" %(self.name,target.name, hit)
-            C[target.name].kill(C[self.name])
+                elif C[target.name].health > 0:
+                        hit = randint(1,20)
+                        target.health -= hit
+                        print '%s hits %s for %s' %(self.name,target.name,hit)
+                C[target.name].kill(C[self.name])
     def IsDead(self):
         if self.health <= 0:
             return True
@@ -39,29 +36,26 @@ class Hero:
             if self.health <= 0 :
                 print '%s is dead' %self.name
                 return False
+                break
             elif C[target.name].health <=0:
                 return False
+                break
             else:
-                if self.health > 0 or C[target.name].health > 0:
-                    if C[target.name].health <= 50:
+                if self.health > 0:
                         hit = randint(1,20)
                         target.health -= hit
                         print '%s hits %s for %s' %(self.name,target.name,hit)
-                    elif C[target.name].health > 50:
-                        hit = randint(1,50)
-                        if hit >= 40:
-                            C[target.name].health -= hit
-                            print "%s critically hits %s for %s" %(self.name,target.name,hit)
-                        else:
-                            C[target.name].health -= hit
-                            print "%s hits %s for %s" %(self.name,target.name, hit)
-            C[target.name].kill(C[self.name])
+                elif C[target.name].health > 0:
+                        hit = randint(1,20)
+                        target.health -= hit
+                        print '%s hits %s for %s' %(self.name,target.name,hit)
+                C[target.name].kill(C[self.name])
     def IsDead(self):
         if self.health <= 0:
             return True
         else:
             return False
-Alice = Dragon('Alice',30)
+Alice = Dragon('Alice',10)
 Fahad = Hero('Fahad', 30)
 C = {'Alice':Alice,'Fahad':Fahad}
 if __name__ == '__main__':
