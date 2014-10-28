308a6faf867bc066bffeae6f12cd9daf7a46de85
HolyMotherofPizza <q8mind@hotmail.com>
2014-10-28 22:12:56 +0300

    Revert "updated code"
    This reverts commit 7381fe5ad34dd8b8b52224d516acaef79bceab16.

--- dragon slaying testing.py
+++ dragon slaying testing.py
@@ -4,17 +4,15 @@ class Dragon:
         self.name = name
         self.health = health
     def kill(self,target): 
-        if self.health <= 0 :
+        if C[self.name].IsDead == True:
             print '%s is dead' %self.name
-        elif C[target.name].health <=0:
-                print '%s has died' %target.name
-        else:
-            while self.health > 0 or C[target.name].health > 0:
+        elif self.IsDead == False:
+            while C[target.name].IsDead() == False:
                 if C[target.name].health <= 50:
                     hit = randint(1,20)
                     target.health -= hit
                     print '%s hits %s for %s' %(self.name,target.name,hit)
-                elif C[target.name].health > 50:
+                else:
                     hit = randint(1,50)
                     if hit >= 40:
                         C[target.name].health -= hit
@@ -22,13 +20,9 @@ class Dragon:
                     else:
                         C[target.name].health -= hit
                         print "%s hits %s for %s" %(self.name,target.name, hit)
-                    C[target.name].kill(C[self.name])
-                elif self.health <= 0 :
-                    print '%s is dead' %self.name
-                    break
-                elif C[target.name].health <=0:
+                if C[target.name].IsDead():
                     print '%s has died' %target.name
-                    break
+            C[target.name].kill(C[self.name])
     def IsDead(self):
         if self.health <= 0:
             return True
@@ -39,17 +33,15 @@ class Hero:
         self.name = name
         self.health = health
     def kill(self,target): 
-        if self.health <= 0 :
+        if C[self.name].IsDead == True:
             print '%s is dead' %self.name
-        elif C[target.name].health <=0:
-                print '%s has died' %target.name
-        else:
-            while self.health > 0 or C[target.name].health > 0:
+        elif self.IsDead == False:
+            while C[target.name].IsDead() == False:
                 if C[target.name].health <= 50:
                     hit = randint(1,20)
                     target.health -= hit
                     print '%s hits %s for %s' %(self.name,target.name,hit)
-                elif C[target.name].health > 50:
+                else:
                     hit = randint(1,50)
                     if hit >= 40:
                         C[target.name].health -= hit
@@ -57,21 +49,16 @@ class Hero:
                     else:
                         C[target.name].health -= hit
                         print "%s hits %s for %s" %(self.name,target.name, hit)
-                    C[target.name].kill(C[self.name])
-                elif self.health <= 0 :
-                    print '%s is dead' %self.name
-                    break
-                elif C[target.name].health <=0:
+                if C[target.name].IsDead():
                     print '%s has died' %target.name
-                    break
+            C[target.name].kill(C[self.name])
     def IsDead(self):
         if self.health <= 0:
             return True
         else:
             return False
-Alice = Dragon('Alice',100)
+Alice = Dragon('Alice',200)
 Fahad = Hero('Fahad', 100)
 C = {'Alice':Alice,'Fahad':Fahad}
 if __name__ == '__main__':
-    Alice.kill(Fahad)
-
+    Fahad.kill(Alice)
